4f230a395329c3f8af736ea8a156554b23f08dbe
Ewebajo-Oluwaseyi <seyiewebajo@gmail.com>
2021-02-03 00:17:28 +0100

    first commit

--- server.js
+++ server.js
@@ -12,19 +12,14 @@ connectDB();
 //Init middleware
 app.use(express.json({extended: false}));
 
+app.get('/', (req,res) => res.json({msg: 'Welcome to Book log'}))
+
+
 
 //routes
 app.use('/api/logs', require('./routes/logs'));
 app.use('/api/loggers', require('./routes/loggers'));
 
-
-//serve static assets in production
-if(process.env.NODE_ENV === 'production'){
-    //set static folder
-    app.use(express.static('book-store-inventory/build'));
-
-    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'book-store-inventory', 'build', 'index.html')))
-}
 
 const PORT = process.env.PORT || 5000;
 

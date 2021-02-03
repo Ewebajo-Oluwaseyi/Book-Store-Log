340aef7de3ea93439beaa5467ee2ebaedf924b87
Ewebajo-Oluwaseyi <seyiewebajo@gmail.com>
2021-02-03 09:21:30 +0100

    Prepare for deploy

--- server.js
+++ server.js
@@ -12,13 +12,20 @@ connectDB();
 //Init middleware
 app.use(express.json({extended: false}));
 
-app.get('/', (req,res) => res.json({msg: 'Welcome to Book log'}))
+/*app.get('/', (req,res) => res.json({msg: 'Welcome to Book log'}))*/
 
 
 
 //routes
 app.use('/api/logs', require('./routes/logs'));
 app.use('/api/loggers', require('./routes/loggers'));
+
+//serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('bookStoreInventory/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'bookStoreInventory', 'build', 'index.html')));
+}
 
 
 const PORT = process.env.PORT || 5000;

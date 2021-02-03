3843e53562722e4a9464fb73296008fc1f965dc0
Ewebajo-Oluwaseyi <seyiewebajo@gmail.com>
2021-02-03 18:16:50 +0100

    changed

--- server.js
+++ server.js
@@ -22,9 +22,9 @@ app.use('/api/loggers', require('./routes/loggers'));
 
 //serve static assets in production
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('bookStoreInventory/build'));
+    app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'bookStoreInventory', 'build', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
 
 

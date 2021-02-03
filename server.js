f011828acb19b77bb13ec3bb80382a34c44874e4
Ewebajo-Oluwaseyi <seyiewebajo@gmail.com>
2021-02-04 00:23:37 +0100

    initial commit

--- server.js
+++ server.js
@@ -21,11 +21,11 @@ app.use('/api/logs', require('./routes/logs'));
 app.use('/api/loggers', require('./routes/loggers'));
 
 //serve static assets in production
-if(process.env.NODE_ENV === 'production'){
+/*if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
-}
+}*/
 
 
 const PORT = process.env.PORT || 5000;
